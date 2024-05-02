724ae70bf4a58fdbc9361bed2779027b704fc3bc
Gracie Stephenson <144754819+egstep722@users.noreply.github.com>
2024-05-02 11:25:38 -0400

    test

--- app/src/main/java/com/example/mytestapp/Preferences.java
+++ app/src/main/java/com/example/mytestapp/Preferences.java
@@ -1,89 +1,14 @@
 package com.example.mytestapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.example.mytestapp.databinding.ActivityMainBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.Firebase;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class Preferences extends AppCompatActivity {
-
-    TextInputEditText uploadFirstName, uploadLastName, uploadAge, uploadMajor;
-    Button saveButton;
-    FirebaseDatabase db;
-    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
-
-        uploadFirstName = findViewById(R.id.firstName);
-        uploadLastName = findViewById(R.id.lastName);
-        uploadAge = findViewById(R.id.userAge);
-        uploadMajor = findViewById(R.id.userMajor);
-        saveButton = findViewById(R.id.savePreferences);
-
-        reference = FirebaseDatabase.getInstance().getReference();
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String firstName, lastName, age, major;
-
-                firstName = String.valueOf(uploadFirstName.getText());
-                lastName = String.valueOf(uploadLastName.getText());
-                age = String.valueOf(uploadAge.getText());
-                major = String.valueOf(uploadMajor.getText());
-
-                if (TextUtils.isEmpty(firstName)){
-                    Toast.makeText(Preferences.this, "Enter first name", Toast.LENGTH_SHORT).show();
-                }
-                if (TextUtils.isEmpty(lastName)){
-                    Toast.makeText(Preferences.this, "Enter last name", Toast.LENGTH_SHORT).show();
-                }
-
-                if (TextUtils.isEmpty(age)){
-                    Toast.makeText(Preferences.this, "Enter age", Toast.LENGTH_SHORT).show();
-                }
-                if (TextUtils.isEmpty(major)){
-                    Toast.makeText(Preferences.this, "Enter major", Toast.LENGTH_SHORT).show();
-                }
-
-                Users user = new Users(firstName, lastName, age, major);
-
-                reference.child("users").setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()){
-                            Toast.makeText(getApplicationContext(), "Preferences Successfully Updated.", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                        else {
-                            Toast.makeText(Preferences.this, "Update failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-
-
-            }
-        });
     }
-
 }
--- app/src/main/java/com/example/mytestapp/Profile.java
+++ app/src/main/java/com/example/mytestapp/Profile.java
@@ -1,7 +1,5 @@
 package com.example.mytestapp;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,33 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class Profile extends Fragment {
-
-    Activity context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        context = getActivity();
-
         return inflater.inflate(R.layout.fragment_profile, container, false);
-
     }
-
-    public void onStart(){
-        super.onStart();
-        Button btn = (Button) context.findViewById(R.id.buttonPreferences);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, Preferences.class);
-                startActivity(intent);
-            }
-        });
-    }
-
 }
