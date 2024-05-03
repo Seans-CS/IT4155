294dbd970696bd2f478cdc1ac9dd4da10c4ff8c0
Gracie Stephenson <144754819+egstep722@users.noreply.github.com>
2024-05-02 22:09:12 -0400

    Revert "test"
    This reverts commit 724ae70bf4a58fdbc9361bed2779027b704fc3bc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,5 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-<<<<<<< HEAD
     id("com.android.application") version "8.2.1" apply false
-=======
-    id("com.android.application") version "8.2.2" apply false
->>>>>>> parent of a147d86 (added user class, and preferences section)
 }
