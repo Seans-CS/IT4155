95174d5bc3b6f9592e61869a41e87ea907aee7b1
Seans-CS <92246830+Seans-CS@users.noreply.github.com>
2024-05-04 15:56:14 -0400

    implementation of google maps api

---
+++ app/src/main/java/com/example/mytestapp/ApartmentsFragment.java
@@ -0,0 +1,43 @@
+package com.example.mytestapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ApartmentsFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_apartments, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in a specific location and move the camera
+        LatLng apartmentLocation = new LatLng(-34, 151); // Example coordinates
+        mMap.addMarker(new MarkerOptions().position(apartmentLocation).title("Apartment"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(apartmentLocation));
+    }
+}
