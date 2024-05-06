83a586864b3e011b95979044e96bf99f08cbe7df
Aaron St. Germaine <astgerma@uncc.edu>
2024-05-06 11:46:49 -0400

    updated profile fragment to match user details and made some UI changes

--- app/src/main/java/com/example/mytestapp/MainActivity.java
+++ app/src/main/java/com/example/mytestapp/MainActivity.java
@@ -20,8 +20,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity  extends AppCompatActivity {
 
     FirebaseAuth auth;
-    Button button;
-    TextView textView;
+
     FirebaseUser user;
 
     ViewPager2 viewPager2;
@@ -34,8 +33,6 @@ public class MainActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
 
         if (user == null){
@@ -43,20 +40,6 @@ public class MainActivity  extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
-        else {
-            textView.setText(user.getEmail());
-        }
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
 
 
       bottomNavigationView = findViewById(R.id.bottomNav);
